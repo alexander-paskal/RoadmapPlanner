00396d57f4712a8552f3f8e3c1ef8abcd0987975
unduwapk <ukandage@ucsd.edu>
2023-06-09 19:31:58 -0700

    merging with main

---
+++ stuff.py
@@ -0,0 +1,14 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def main():
+    cspace = np.load("cspace_hw3.npy")
+    print(cspace.shape)
+
+
+
+if __name__ == "__main__":
+    main()
+
+    
